3f11ffac78afd6ab4b3551ddd437ea397a0e8418
Roilan <roilan14@gmail.com>
2015-04-10 18:45:58 -0400

    reqwest getData function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 (function() {
+    // key temporarily pulls from apiKey.js
+    // will eventually equal to user unique API key
+    var key = apiKey;
 
+    // reqwest variables
+    var r = reqwest;
+    var url = 'https://api.digitalocean.com/v2/';
+
+    function getData(action) {
+        r({
+            url: url + action,
+            method: 'get',
+            headers: {
+              'Authorization': 'Bearer ' + key
+            },
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+    }
+
+    getData('droplets');
 })();
